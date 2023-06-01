1bbdf08d8edcfc7a78a4ac9e468121930d2bc60c
ericc70 <eric.c@codeur.online>
2023-06-01 23:43:02 +0200

    refactorise DomainChecker

--- src/Class/DomainChecker.php
+++ src/Class/DomainChecker.php
@@ -4,28 +4,42 @@ namespace  Ericc70\ValidationUtils\Class;
 
 use InvalidArgumentException;
 
-class DomainChecker {
+class DomainChecker
+{
     private $bannedDomains;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->bannedDomains = $this->loadBannedDomains();
     }
 
-    private function loadBannedDomains() {
-        $bannedDomainsFile = realpath(__DIR__ . '/../Data/forbidenDomainEmail.txt');
-       
+    private function loadBannedDomains()
+    {
+        $bannedDomainsFile = $this->getBannedDomainsFilePath();
+
         if (file_exists($bannedDomainsFile)) {
             $domains = file($bannedDomainsFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-          return array_map('trim', $domains);
-      
+            return array_map('trim', $domains);
         } else {
             throw new InvalidArgumentException("Banned domains file not found: $bannedDomainsFile");
         }
     }
 
-    public function isDomainBanned($email) {
-        $domain = substr(strrchr($email, "@"), 1);
-   
+    private function getBannedDomainsFilePath()
+    {
+        $baseDir = realpath(__DIR__ . '/../Data');
+        return $baseDir . '/forbidenDomainEmail.txt';
+    }
+
+    public function isDomainBanned($email)
+    {
+        $domain = $this->extractDomainFromEmail($email);
         return in_array($domain, $this->bannedDomains);
+    }
+
+    private function extractDomainFromEmail($email)
+    {
+        $parts = explode('@', $email);
+        return end($parts);
     }
 }
