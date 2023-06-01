dd63c25f593ad597aa24d51a4b3ca3a73a1da4a3
ericc70 <eric.c@codeur.online>
2023-06-01 23:38:57 +0200

    refactorise RezgexCollection

--- src/Class/RegexCollection.php
+++ src/Class/RegexCollection.php
@@ -6,18 +6,24 @@ use Exception;
 
 class RegexCollection
 {
+    private static $regexes = [
+        'alphaNumeric' => '/^[a-zA-Z0-9]+$/',
+        'email' => '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
+        'url' => '/^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(\/\S*)?$/',
+        // Ajoutez d'autres regex réutilisables ici...
+    ];
+
     public static function getRegex($name)
     {
-        $regexes = [
-            'alphaNumeric' => '/^[a-zA-Z0-9]+$/',
-            'email' => '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
-            // Ajoutez d'autres regex réutilisables ici...
-        ];
-
-        if (isset($regexes[$name])) {
-            return $regexes[$name];
+        if (isset(self::$regexes[$name])) {
+            return self::$regexes[$name];
         }
 
         throw new Exception("Regex '$name' not found.");
+    }
+
+    public static function addRegex($name, $regex)
+    {
+        self::$regexes[$name] = $regex;
     }
 }
